4d74613050bd7ac65844d0b7080e75b9c6104183
renechoi <renechoi90@gmail.com>
2023-04-11 23:37:50 +0900

    refactor: auditingfields의 deletedYn 값 수정 - not null 조건에 따라 초기화시 동시에 값이 들어가야 하므로 default 값 설정 - 추상 클래스이므로 구체 클래스에서 이를 덮어쓸 수 있으므로 문제되는 부분은 아닌 것으로 판단

--- service/src/main/java/com/petkpetk/service/common/AuditingFields.java
+++ service/src/main/java/com/petkpetk/service/common/AuditingFields.java
@@ -26,8 +26,8 @@ public abstract class AuditingFields {
 
 	// TODO: null 값 설정에 대한 재고 필요, length 설정 고려
 
-	@Column(nullable = false, columnDefinition = "varchar(1) default 'N'")
-	protected String deletedYn;
+	@Column(nullable = false)
+	protected String deletedYn = "N";
 
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
 	@CreatedDate
