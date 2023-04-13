b96d60c408f7980438c432099260eb484ece0980
hyejin0662 <hyejin0776@gmail.com>
2023-04-13 11:47:00 +0900

    feat: Order Repository 및 Service init commit

---
+++ service/src/main/java/com/petkpetk/service/domain/shopping/service/order/OrderService.java
@@ -0,0 +1,14 @@
+package com.petkpetk.service.domain.shopping.service.order;
+
+import org.springframework.stereotype.Service;
+
+import com.petkpetk.service.domain.shopping.repository.order.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+	private final OrderRepository repository;
+}
