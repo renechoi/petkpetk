4362d2947a3be0f2c0048378b9db484b608cd988
renechoi <renechoi90@gmail.com>
2023-04-13 17:21:24 +0900

    fix: update 관련 주석 추가

--- service/src/main/java/com/petkpetk/service/domain/user/dto/UserAccountDto.java
+++ service/src/main/java/com/petkpetk/service/domain/user/dto/UserAccountDto.java
@@ -36,17 +36,19 @@ public class UserAccountDto {
 		return EntityAndDtoConverter.convertToDto(userAccount, UserAccountDto.class);
 	}
 
-	public static UserAccountDto of(Long id, String email, String password, String name, String nickname, Address address, String profileImage, SignUpProvider signUpProvider, Set<RoleType> roles){
-		return new UserAccountDto(
-			id,
-			email,
-			password,
-			name,
-			nickname,
-			address,
-			profileImage,
-			signUpProvider,
-			roles
-		);
+	public static UserAccountDto of(Long id, String email, String password, String name, String nickname,
+		Address address, String profileImage, SignUpProvider signUpProvider, Set<RoleType> roles) {
+		return new UserAccountDto(id, email, password, name, nickname, address, profileImage, signUpProvider, roles);
 	}
+
+	// TODO : update 수행을 dto가 아닌 entity에서 직접 담당하게 하는 것이 맞는 것으로 판단. 이점에 대한 추후 재고 필요.
+	// public void updateUserAccount(UserAccount userAccount) {
+	// 	userAccount.setEmail(this.email);
+	// 	userAccount.setPassword(this.password);
+	// 	userAccount.setName(this.name);
+	// 	userAccount.setNickname(this.nickname);
+	// 	userAccount.setAddress(this.address);
+	// 	userAccount.setProfileImage(this.profileImage);
+	// 	userAccount.setRoles(this.roles);
+	// }
 }
