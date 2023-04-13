084d0b8ed9675063e7735290560cade5c1d18582
ansungmin0518 <asm19920518@gmail.com>
2023-04-13 12:36:39 +0900

    feat: HashTag entity 및 Dto init commit

---
+++ service/src/main/java/com/petkpetk/service/domain/board/dto/HashTagDto.java
@@ -0,0 +1,43 @@
+package com.petkpetk.service.domain.board.dto;
+
+import java.time.LocalDate;
+
+import com.petkpetk.service.common.converter.EntityAndDtoConverter;
+import com.petkpetk.service.domain.board.entity.HashTag;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HashTagDto {
+
+	private Long hashTagId;
+	private String hashTagName;
+	private LocalDate createdAt;
+	private LocalDate modifiedAt;
+	private LocalDate deletedAt;
+	private String deleteYn;
+
+	public HashTag toEntity() {
+		return EntityAndDtoConverter.convertToEntity(this, HashTag.class);
+	}
+
+	public static HashTagDto fromEntity(HashTag hashTag) {
+		return EntityAndDtoConverter.convertToDto(hashTag, HashTagDto.class);
+	}
+
+	public static HashTagDto of(Long hashTagId, String hashTagName, LocalDate createdAt, LocalDate modifiedAt,
+		LocalDate deletedAt, String deleteYn) {
+		return new HashTagDto(
+		hashTagId,
+		hashTagName,
+		createdAt,
+		modifiedAt,
+		deletedAt,
+		deleteYn
+		);
+	}
+}
