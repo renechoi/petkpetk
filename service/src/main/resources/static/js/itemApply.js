3414402f5d2f415a78cf9d5047b27c816cc54add
hyejin0662 <hyejin0776@gmail.com>
2023-04-15 22:57:19 +0900

    feat: [예은] itemApply.css, js, html 구현

--- service/src/main/resources/static/js/itemApply.js
+++ service/src/main/resources/static/js/itemApply.js
@@ -1,16 +1,61 @@
 var addItemContainer = document.getElementById("addItemContainer");
 
 
-$("#file").on('change', function () {
-    var fileName = $("#file").val();
-    $(".upload-name").val(fileName);
-});
-
 function getFileName(num) {
     var fileName = $("#itemFile" + num).val();
     var cleanName = fileName.substring(12);
     $(".fileName" + num).val(cleanName);
 }
+
+function removeImage(num) {
+    $("#itemFile" + num).val(null);
+    if (num == 1) {
+        $(".fileName" + num).val('대표 이미지')
+    } else {
+        $(".fileName" + num).val('첨부 파일')
+    }
+}
+
+
+var addForm = document.getElementById("addForm");
+var firstImage = document.getElementById("firstImage");
+var addItemName = document.getElementById("addItemName");
+var addItemPrice = document.getElementById("itemPrice");
+var addAmount = document.getElementById("itemAmount");
+var addItemDetail = document.getElementById("itemDetail");
+
+addForm.addEventListener('submit', function (event) {
+
+    if (addItemName == null || addItemName.value == "") {
+        alert("상품명을 입력해주세요.");
+        event.preventDefault();
+        return false;
+    }
+
+    if (addItemPrice == null || addItemPrice.value == "") {
+        alert("상품 가격을 정해주세요.");
+        event.preventDefault();
+        return false;
+    }
+
+    if (addAmount == null || addAmount.value == "") {
+        alert("상품 재고를 입력해주세요.");
+        event.preventDefault();
+        return false;
+    }
+
+    if (addItemDetail == null || addItemDetail.value == "") {
+        alert("상품 상세 정보를 입력해주세요.");
+        event.preventDefault();
+        return false;
+    }
+
+    if (firstImage == null || firstImage.value == "대표 이미지") {
+        alert("대표 이미지를 정해주세요.");
+        event.preventDefault();
+        return false;
+    }
+});
 
 
 const AC = document.querySelector(".addItemContainer");
