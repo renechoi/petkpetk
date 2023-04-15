3e613839bd46c38d701074f715141a9e13c9b5cb
hyejin0662 <hyejin0776@gmail.com>
2023-04-15 22:42:54 +0900

    rename: SellerCalculationController, SellerCalculationService 수정

--- service/src/main/java/com/petkpetk/service/domain/shopping/service/calculation/SellerCalculationService.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/service/calculation/SellerCalculationService.java
@@ -1,14 +1,14 @@
-package com.petkpetk.service.domain.shopping.service.calculation;
-
-import org.springframework.stereotype.Service;
-
-import com.petkpetk.service.domain.shopping.repository.calculation.SellerCalculationRepository;
-
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
-public class SellerCalculationService {
-
-	private final SellerCalculationRepository sellerCalculationRepository;
-}
+// package com.petkpetk.service.domain.shopping.service.calculation;
+//
+// import org.springframework.stereotype.Service;
+//
+// import com.petkpetk.service.domain.shopping.repository.calculation.SellerCalculationRepository;
+//
+// import lombok.RequiredArgsConstructor;
+//
+// @Service
+// @RequiredArgsConstructor
+// public class SellerCalculationService {
+//
+// 	private final SellerCalculationRepository sellerCalculationRepository;
+// }
