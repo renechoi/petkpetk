29773a5b366b149c178afda789fc143d68133264
renechoi <renechoi90@gmail.com>
2023-04-15 23:46:25 +0900

    feat: oauth2 인증 서비스 및 설정 구현

--- service/src/main/java/com/petkpetk/service/config/security/SecurityConfig.java
+++ service/src/main/java/com/petkpetk/service/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.petkpetk.service.config.security;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.petkpetk.service.domain.user.dto.security.UserAccountPrincipal;
+import com.petkpetk.service.domain.user.service.SocialAccountService;
 import com.petkpetk.service.domain.user.service.UserAccountService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,9 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final OAuth2Config oAuth2Config;
+	private final SocialAccountService SocialAccountService;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -36,6 +41,15 @@ public class SecurityConfig {
 				.usernameParameter("email")
 				.passwordParameter("password"))
 
+			.oauth2Login(Customizer.withDefaults())
+			.oauth2Login(oauth2 -> oauth2.clientRegistrationRepository(oAuth2Config.clientRegistrationRepository())
+				.authorizedClientService(oAuth2Config.oAuth2AuthorizedClientService())
+				.userInfoEndpoint(
+					user -> user
+						.userService(SocialAccountService)
+				)
+			)
+
 			.logout(
 				logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/"))
 
@@ -50,5 +64,4 @@ public class SecurityConfig {
 			.map(UserAccountPrincipal::from)
 			.orElseThrow(() -> new UsernameNotFoundException("회원을 찾을 수 없습니다"));
 	}
-
 }
