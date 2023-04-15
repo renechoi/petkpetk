a7f57e3201c4f686eaa4e43c9c9c9cbbd5ca9950
hyejin0662 <hyejin0776@gmail.com>
2023-04-15 19:48:38 +0900

    feat: Order entity update

--- service/src/main/java/com/petkpetk/service/domain/shopping/entity/order/Order.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/entity/order/Order.java
@@ -1,9 +1,19 @@
 package com.petkpetk.service.domain.shopping.entity.order;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.petkpetk.service.domain.shopping.constant.OrderStatus;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,8 +30,14 @@ public class Order {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long orderId;
+	@Enumerated(EnumType.STRING)
+	private OrderStatus orderStatus;
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+	@ToString.Exclude
+	private List<OrderItem> orderItems = new ArrayList<>();
 
 	private Long userId;
+
 	private Long productId;
 	private Long amount;
 	private double salePercent;
