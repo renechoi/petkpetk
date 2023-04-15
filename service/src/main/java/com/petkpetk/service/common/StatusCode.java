3d1957635cbb5cb166aa260d5aa877aea6960beb
renechoi <renechoi90@gmail.com>
2023-04-15 23:15:15 +0900

    feat: OAuth2 인증을 구현에 따른 추가 메시지 구현

--- service/src/main/java/com/petkpetk/service/common/StatusCode.java
+++ service/src/main/java/com/petkpetk/service/common/StatusCode.java
@@ -13,7 +13,9 @@ public enum StatusCode {
 
 	USER_DUPLICATE("중복 회원이 존재합니다."),
 
-	USER_NOT_FOUND("요청한 회원을 찾을 수 없습니다.")
+	USER_NOT_FOUND("요청한 회원을 찾을 수 없습니다."),
+
+	OAUTH2_PROVIDER_ERROR("소셜 로그인 진행중 오류가 발생했습니다. 다시 시도해주세요.")
 
 
 
