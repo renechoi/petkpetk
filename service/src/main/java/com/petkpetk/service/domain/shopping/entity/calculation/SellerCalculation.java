d0f259a2380d7e292ae05730cfb3ff82ff75b2d9
hyejin0662 <hyejin0776@gmail.com>
2023-04-18 17:00:53 +0900

    feat: SellerCalculation 필드 및 어노테이션 삽입

--- service/src/main/java/com/petkpetk/service/domain/shopping/entity/calculation/SellerCalculation.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/entity/calculation/SellerCalculation.java
@@ -1,4 +1,37 @@
 package com.petkpetk.service.domain.shopping.entity.calculation;
 
-public class SellerCalculation {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.petkpetk.service.common.AuditingFields;
+import com.petkpetk.service.domain.user.entity.SellerAccount;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+public class SellerCalculation extends AuditingFields {
+	@Id
+	@Column(name ="calculation_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToMany(mappedBy = "seller_account")
+	private SellerAccount sellerAccount;
+
+
+
+
 }
