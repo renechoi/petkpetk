ec94105b882fd58894cbccbb6cdf91a566d7ba6f
hyejin0662 <hyejin0776@gmail.com>
2023-04-18 16:59:47 +0900

    feat: PaymentDto 메서드 추가

--- service/src/main/java/com/petkpetk/service/domain/shopping/dto/payment/PaymentDto.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/dto/payment/PaymentDto.java
@@ -5,6 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.petkpetk.service.config.converter.EntityAndDtoConverter;
+import com.petkpetk.service.domain.shopping.dto.delivery.DeliveryDto;
+import com.petkpetk.service.domain.shopping.entity.delivery.Delivery;
+import com.petkpetk.service.domain.shopping.entity.order.Order;
+import com.petkpetk.service.domain.shopping.entity.payment.Payment;
+import com.petkpetk.service.domain.shopping.service.delivery.DeliveryService;
+import com.petkpetk.service.domain.user.entity.UserAccount;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +22,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PaymentDto {
 
-	private Long payId;
+	private Long id;
 	private Long orderId;
 
 	private Long userId;
@@ -23,4 +31,28 @@ public class PaymentDto {
 	private Long totalCost;
 	private String payToken; // *****
 	private String payApp; // *****
+
+	public Payment toEntity() {
+		return EntityAndDtoConverter.convertToEntity(this, Payment.class);
+	}
+	public static PaymentDto fromEntity(Payment payment) {
+		return EntityAndDtoConverter.convertToDto(payment, PaymentDto.class);
+	}
+
+
+
+	public static PaymentDto of(Long id, Long orderId, Long userId, Long productId, Long totalCost, String payToken,
+		String payApp) {
+		return new PaymentDto(
+			id,
+			orderId,
+			userId,
+			productId,
+			totalCost,
+			payToken,
+			payApp
+		);
+	}
+
+
 }
