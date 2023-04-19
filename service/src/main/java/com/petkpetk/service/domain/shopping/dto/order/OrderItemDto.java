67a73e4060cddbffb71aa64c102e6d6376415ab1
hyejin0662 <hyejin0776@gmail.com>
2023-04-19 21:16:45 +0900

    feat: 추가 메서드 구현

--- service/src/main/java/com/petkpetk/service/domain/shopping/dto/order/OrderItemDto.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/dto/order/OrderItemDto.java
@@ -2,7 +2,10 @@ package com.petkpetk.service.domain.shopping.dto.order;
 
 import java.time.LocalDateTime;
 
+import com.petkpetk.service.config.converter.EntityAndDtoConverter;
 import com.petkpetk.service.domain.shopping.constant.OrderStatus;
+import com.petkpetk.service.domain.shopping.entity.order.Order;
+import com.petkpetk.service.domain.shopping.entity.order.OrderItem;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,7 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderItemDto {
-	private Long orderItemId;
+	private Long id;
 	private Long payId;
 	private Long orderId;
 	private Long userId;
@@ -23,8 +26,27 @@ public class OrderItemDto {
 	private LocalDateTime startTime;
 	private LocalDateTime cancelTime;
 
+	public OrderItem toEntity() {
+		return EntityAndDtoConverter.convertToEntity(this, OrderItem.class);
+	}
 
+	public static OrderItemDto fromEntity(Order order) {
+		return EntityAndDtoConverter.convertToDto(order, OrderItemDto.class);
+	}
 
+	public static OrderItemDto of(Long id, Long payId, Long orderId, Long userId, Long productId,
+		OrderStatus orderStatus,
+		LocalDateTime startTime, LocalDateTime cancelTime) {
+		return new OrderItemDto(
+			id,
+			payId,
+			orderId,
+			userId,
+			productId,
+			orderStatus,
+			startTime,
+			cancelTime
+		);
 
-
+	}
 }
