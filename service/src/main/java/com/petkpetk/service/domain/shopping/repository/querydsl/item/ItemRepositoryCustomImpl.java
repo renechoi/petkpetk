dc35aac45eee7636c025cae83f4765bf247dc679
KangCuty <goldbaby1@naver.com>
2023-04-24 17:15:42 +0900

    feat: item( 상품 ) ItemRepositoryCustom, ItemRepositoryCustomImpl 수정

--- service/src/main/java/com/petkpetk/service/domain/shopping/repository/querydsl/item/ItemRepositoryCustomImpl.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/repository/querydsl/item/ItemRepositoryCustomImpl.java
@@ -56,7 +56,7 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
 			dateTime = dateTime.minusMonths(6);
 		}
 
-		return QItem.item.registeredAt.after(dateTime);
+		return QItem.item.createdAt.after(dateTime);
 	}
 
 	private BooleanExpression searchByLike(String searchBy, String searchQuery) {
@@ -123,7 +123,7 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
 					item.itemName,
 					item.itemDetail,
 					item.itemStatus,
-					item.sellerAccount.businessName,
+					item.userAccount.businessName,
 					itemImage.imageUrl,
 					item.price
 				)
@@ -165,7 +165,7 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
 					itemImage.imageUrl,
 					item.price,
 					item.createdBy,
-					item.registeredAt
+					item.createdAt
 				)
 			)
 			.from(itemImg)
