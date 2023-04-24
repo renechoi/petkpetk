c4986f789ba8a8e184971944bcf5cd5e20c1a1b9
KangCuty <goldbaby1@naver.com>
2023-04-24 17:05:47 +0900

    feat: item( 상품 ) itemDto, ItemImageDto, ItemRegisterRequest, ItemRequest, ItemResponse,  ItemSearchDto 수정

--- service/src/main/java/com/petkpetk/service/domain/shopping/dto/item/request/ItemRegisterRequest.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/dto/item/request/ItemRegisterRequest.java
@@ -1,16 +1,17 @@
-package com.petkpetk.service.domain.shopping.dto.item.response;
+package com.petkpetk.service.domain.shopping.dto.item.request;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import com.petkpetk.service.config.converter.EntityAndDtoConverter;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.petkpetk.service.domain.shopping.constant.ItemStatus;
 import com.petkpetk.service.domain.shopping.dto.item.ItemImageDto;
 import com.petkpetk.service.domain.shopping.entity.item.Item;
+import com.petkpetk.service.domain.shopping.entity.item.ItemImage;
 import com.petkpetk.service.domain.user.entity.UserAccount;
 
 import lombok.AllArgsConstructor;
@@ -20,7 +21,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ItemResponse {
+public class ItemRegisterRequest {
 	private Long id;
 
 	@NotBlank(message = "상품명을 입력해주세요.")
@@ -39,21 +40,24 @@ public class ItemResponse {
 
 	private UserAccount userAccount;
 
+	private List<MultipartFile> images = new ArrayList<>();
+
 	private List<ItemImageDto> itemImageDtos = new ArrayList<>();
 
-	public Item toEntity() {
+
+	public Item toEntity(List<ItemImage> images) {
 		return Item.of(
 			this.itemName,
 			this.price,
 			this.itemAmount,
 			this.itemDetail,
 			this.itemStatus,
-			null, // TODO : null 처리
+			images,
 			this.userAccount
 		);
 	}
 
-	public ItemResponse(String itemName, Long price, Long itemAmount, String itemDetail,
+	public ItemRegisterRequest(String itemName, Long price, Long itemAmount, String itemDetail,
 		ItemStatus itemStatus, UserAccount userAccount) {
 		this.itemName = itemName;
 		this.price = price;
@@ -63,28 +67,13 @@ public class ItemResponse {
 		this.userAccount = userAccount;
 	}
 
-	public static ItemResponse of(String itemName, Long price, Long itemAmount, String itemDetail,
+	public static ItemRegisterRequest of(String itemName, Long price, Long itemAmount, String itemDetail,
 		ItemStatus itemStatus, UserAccount userAccount) {
-		return new ItemResponse(itemName, price, itemAmount, itemDetail, itemStatus, userAccount);
+		return new ItemRegisterRequest(itemName, price, itemAmount, itemDetail, itemStatus, userAccount);
 	}
 
-	public static ItemResponse from(Item item) {
-		ItemResponse itemResponse = EntityAndDtoConverter.convertToDto(item, ItemResponse.class);
-		itemResponse.setItemImageDtos(item.getImages().stream().map(ItemImageDto::from).collect(Collectors.toList()));
-		return itemResponse;
+	public static ItemRegisterRequest of(Item item) {
+		return ItemRegisterRequest.of(item.getItemName(), item.getPrice(), item.getItemAmount(),
+			item.getItemDetail(), item.getItemStatus(), item.getUserAccount());
 	}
-
-	public static ItemResponse of(Item itemEntity, List<ItemImageDto> itemImageDtos) {
-		return new ItemResponse(
-			itemEntity.getId(),
-			itemEntity.getItemName(),
-			itemEntity.getPrice(),
-			itemEntity.getItemAmount(),
-			itemEntity.getItemDetail(),
-			itemEntity.getItemStatus(),
-			itemEntity.getUserAccount(),
-			itemImageDtos
-		);
-	}
-
 }
