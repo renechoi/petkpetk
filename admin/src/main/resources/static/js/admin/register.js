30c0ce3df867e457263c8fc26fe1fb566eeb0d6a
renechoi <115696395+renechoi@users.noreply.github.com>
2023-04-26 18:04:08 +0900

    feat: 회원가입 양식 이메일 인증 구현

--- admin/src/main/resources/static/js/admin/register.js
+++ admin/src/main/resources/static/js/admin/register.js
@@ -1,4 +1,9 @@
-$(document).ready(function() {
+
+
+
+$(document).ready(function () {
+
+    let timer;
 
     // name 필드가 비어 있는지 체크하는 함수
     function validateName() {
@@ -20,38 +25,80 @@ $(document).ready(function() {
         }
     }
 
+    function validateEmail(email) {
+        const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return re.test(String(email).toLowerCase());
+    }
 
 
-
-
-
-    // 중복 이메일 검증을 실행하는 함수
-
-
-    $('input[name="email"]').on('blur', function() {
+    // 이메일 유효성 검증 및 중복 이메일 검증을 실행하는 함수
+    $('input[name="email"]').on('blur', function () {
         let email = $(this).val();
+        if (validateEmail(email)) {
+            let csrfToken = $('meta[name="_csrf"]').attr('content');
+            let csrfHeader = $('meta[name="_csrf_header"]').attr('content');
+            let headers = {};
+            headers[csrfHeader] = csrfToken;
+            $.ajax({
+                url: '/check-email',
+                type: 'POST',
+                headers: headers,
+                data: {'email': email},
+                success: function (data) {
+                    if (data) {
+                        $('#email-check-msg').html('사용 가능한 Email입니다.').css('color', 'green');
+                        enableSubmitButton();
+                    } else {
+                        $('#email-check-msg').html('이미 사용 중인 Email입니다.').css('color', 'red');
+                    }
+                },
+                error: function () {
+                    alert('서버 오류로 인해 Email 중복 검사에 실패했습니다.');
+                    enableSubmitButton();
+                }
+            });
+        } else {
+            $('#email-check-msg').html('올바른 이메일 형식이 아닙니다.').css('color', 'red');
+        }
+    });
+
+
+    $('#send-auth-code-btn').on('click', function () {
+        let email = $('#email-input').val();
         let csrfToken = $('meta[name="_csrf"]').attr('content');
         let csrfHeader = $('meta[name="_csrf_header"]').attr('content');
         let headers = {};
         headers[csrfHeader] = csrfToken;
-        $.ajax({
-            url: '/check-email',
-            type: 'POST',
-            headers: headers,
-            data: {'email': email},
-            success: function(data) {
-                if (data) {
-                    $('#email-check-msg').html('사용 가능한 Email입니다.').css('color', 'green');
-                    enableSubmitButton();
-                } else {
-                    $('#email-check-msg').html('이미 사용 중인 Email입니다.').css('color', 'red');
+        if (email) {
+            $(this).prop('disabled', true);
+            const loadingIcon = $('<div>', {class: 'loading-icon'});
+            const spinnerIcon = $('<i>', {class: 'fa fa-spinner fa-spin'});
+            loadingIcon.append(spinnerIcon);
+            $(this).closest('.input-group-append').append(loadingIcon.css({
+                'position': 'absolute',
+                'top': '50%',
+                'left': '50%',
+                'transform': 'translate(-50%, -50%)'
+            }));
+            $.ajax({
+                type: 'POST',
+                url: '/send-verification-code',
+                headers: headers,
+                data: {email: email},
+                success: function (result) {
+                    $('.form-container').show();
+                    startTimer(180, '.form-input-timer');
+                },
+                error: function (xhr, status, error) {
+                    alert('에러 발생');
+                },
+                complete: function () {
+                    loadingIcon.remove();
                 }
-            },
-            error: function() {
-                alert('서버 오류로 인해 Email 중복 검사에 실패했습니다.');
-                enableSubmitButton();
-            }
-        });
+            });
+        } else {
+            alert('이메일을 입력하세요.');
+        }
     });
 
 
@@ -80,18 +127,68 @@ $(document).ready(function() {
         enableSubmitButton();
     });
 
-    $('#check-email-btn').on('click', function() {
-        $('button[type="submit"]').prop('disabled', true);
-        checkEmail();
-    });
-
-    $('input[name="email"]').on('input', function() {
+    $('input[name="email"]').on('input', function () {
         $('#email-check-msg').text('');
         disableSubmitButton();
     });
 
-});
 
+    function startTimer(duration, selector) {
+        timer = setInterval(function () {
+            let minutes = Math.floor(duration / 60);
+            let seconds = duration % 60;
+            $(selector).text(minutes + ':' + (seconds < 10 ? '0' : '') + seconds);
+            duration--;
+            if (duration < 0) {
+                clearInterval(timer);
+                $(selector).text('시간 초과');
+            }
+        }, 1000);
+    }
+
+    // 인증코드 input box에 입력이 있을 때마다 실행되는 함수
+    $('#auth-code-input').on('input', function() {
+        if($(this).val().length === 6) {
+            $('button.form-submit-button').prop('disabled', false);
+            return;
+        }
+        $('button.form-submit-button').prop('disabled', true);
+
+    });
+
+    $('.form-submit-button').on('click', function () {
+        let email = $('#email-input').val();
+        let verificationCode = $('#auth-code-input').val();
+        console.log(verificationCode);
+        let csrfToken = $('meta[name="_csrf"]').attr('content');
+        let csrfHeader = $('meta[name="_csrf_header"]').attr('content');
+        let headers = {};
+        headers[csrfHeader] = csrfToken;
+        if (verificationCode) {
+            $.ajax({
+                type: 'POST',
+                url: '/verify-verification-code',
+                headers: headers,
+                data: {email:email, verificationCode: verificationCode},
+                success: function (result) {
+                    if(result){
+                        clearInterval(timer);
+                        $('.form-input-timer').text('인증 완료');
+                        $('#email-validation-form-text').html('이메일 인증이 완료되었습니다').css('color', 'green');
+                    }else{
+                        alert('인증번호가 일치하지 않습니다.');
+                    }
+                },
+                error: function (xhr, status, error) {
+                    alert('에러 발생');
+                }
+            });
+        }
+    });
+
+
+
+});
 
 
 
