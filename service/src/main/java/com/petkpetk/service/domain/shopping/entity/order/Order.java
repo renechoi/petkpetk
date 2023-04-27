44350fe73c6292c00de5656f00f9626d8768a90c
hyejin0662 <hyejin0776@gmail.com>
2023-04-27 11:38:56 +0900

    feat: Order 생성자 및 of 메서드 구현

--- service/src/main/java/com/petkpetk/service/domain/shopping/entity/order/Order.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/entity/order/Order.java
@@ -1,15 +1,22 @@
 package com.petkpetk.service.domain.shopping.entity.order;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.petkpetk.service.domain.shopping.constant.OrderStatus;
+import com.petkpetk.service.common.AuditingFields;
+import com.petkpetk.service.domain.user.entity.UserAccount;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,25 +29,49 @@ import lombok.ToString;
 @ToString(callSuper = true)
 @NoArgsConstructor
 @Entity
-public class Order {
+public class Order extends AuditingFields {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "order_id")
 	private Long id;
 
-	@Enumerated(EnumType.STRING)
-	private OrderStatus orderStatus;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_account_id")
+	@ToString.Exclude
+	private UserAccount userAccount;
 
-	// @OneToMany(mappedBy = "order_item_id", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-	// @ToString.Exclude
-	// private List<OrderItem> orderItems = new ArrayList<>();
 
-	private Long userId;
+	@OneToMany(mappedBy = "order_item_id", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+	@ToString.Exclude
+	private List<OrderItem> orderItems = new ArrayList<>();
 
-	private Long itemId;
 	private Long amount;
 	private double salePercent;
 	private Long salePrice;
+
+	public Order(UserAccount userAccount, List<OrderItem> orderItems, Long amount, double salePercent) {
+		this.userAccount = userAccount;
+		this.orderItems = orderItems;
+		this.amount = amount;
+		this.salePercent = salePercent;
+	}
+
+	public Order(UserAccount userAccount, List<OrderItem> orderItems, Long amount, Long salePrice) {
+		this.userAccount = userAccount;
+		this.orderItems = orderItems;
+		this.amount = amount;
+		this.salePrice = salePrice;
+	}
+
+	public static Order of(UserAccount userAccount, List<OrderItem> orderItems, Long amount, double salePercent) {
+		return new Order(userAccount, orderItems, amount, salePercent);
+	}
+
+	public static Order of(UserAccount userAccount, List<OrderItem> orderItems, Long amount, Long salePrice) {
+		return new Order(userAccount, orderItems, amount, salePrice);
+	}
+
+
 
 }
