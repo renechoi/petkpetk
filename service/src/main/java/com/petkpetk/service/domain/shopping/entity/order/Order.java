d9d391c3776eaf16acd5b8dbc371dc24cca3fbed
hyejin0662 <hyejin0776@gmail.com>
2023-04-27 13:02:18 +0900

    feat: Order - OrderStatus 필드 추가, addOrderItem, createOrder, getTotalPrice, cancelOrder 메서드 구현

--- service/src/main/java/com/petkpetk/service/domain/shopping/entity/order/Order.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/entity/order/Order.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +18,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.petkpetk.service.common.AuditingFields;
+import com.petkpetk.service.domain.shopping.constant.OrderStatus;
 import com.petkpetk.service.domain.user.entity.UserAccount;
 
 import lombok.Getter;
@@ -42,9 +45,12 @@ public class Order extends AuditingFields {
 	private UserAccount userAccount;
 
 
-	@OneToMany(mappedBy = "order_item_id", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
 	@ToString.Exclude
 	private List<OrderItem> orderItems = new ArrayList<>();
+
+	@Enumerated(EnumType.STRING)
+	private OrderStatus orderStatus;
 
 	private Long amount;
 	private double salePercent;
@@ -72,6 +78,36 @@ public class Order extends AuditingFields {
 		return new Order(userAccount, orderItems, amount, salePrice);
 	}
 
+	public Order(UserAccount userAccount, OrderStatus orderStatus, List<OrderItem> orderItems) {
+		this.userAccount = userAccount;
+		this.orderStatus = orderStatus;
+		this.orderItems = orderItems;
+	}
 
+	public void addOrderItem(OrderItem orderItem){
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
+
+	public static Order createOrder(UserAccount userAccount, List<OrderItem> orderItems){
+		Order order = new Order();
+		order.setUserAccount(userAccount);
+		order.setOrderStatus(OrderStatus.ORDER);
+
+		orderItems.forEach(order::addOrderItem);
+		return order;
+
+	}
+
+	public Long getTotalPrice(){
+		return orderItems.stream().mapToLong(OrderItem::getTotalPrice).sum();
+
+	}
+
+
+	public void cancelOrder(){
+		this.orderStatus = OrderStatus.CANCEL;
+		orderItems.forEach(OrderItem::cancel);
+	}
 
 }
