1ff7d1720517cd914a9658aaaffd0dde07dce5e4
hyejin0662 <hyejin0776@gmail.com>
2023-04-27 12:58:51 +0900

    feat: CartItemDto - 어노테이션 추가

--- service/src/main/java/com/petkpetk/service/domain/shopping/dto/cart/CartItemDto.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/dto/cart/CartItemDto.java
@@ -1,7 +1,12 @@
 package com.petkpetk.service.domain.shopping.dto.cart;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import com.petkpetk.service.config.converter.EntityAndDtoConverter;
 import com.petkpetk.service.domain.shopping.entity.cart.Cart;
+import com.petkpetk.service.domain.shopping.entity.cart.CartItem;
+import com.petkpetk.service.domain.shopping.entity.item.Item;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,19 +18,25 @@ import lombok.NoArgsConstructor;
 public class CartItemDto {
 
 	private Long cartItemId;
-	private Long cartId;
+	private Cart cartId;
+
+	@NotNull(message = "상품 아이디는 필수 입력 값 입니다")
 	private Long itemId;
 
-	public Cart toCart() {
-		return EntityAndDtoConverter.convertToEntity(this, Cart.class);
+	@Min(value = 1, message = "최소 1개 이상 담아주세요")
+	private Long itemCount;
+
+	public CartItem toCart() {
+		return EntityAndDtoConverter.convertToEntity(this, CartItem.class);
 	}
 
 
-	public static CartDto of(Long cartItemId, Long cartId, Long itemId) {
-		return new CartDto(
+	public static CartItemDto of(Long cartItemId, Cart cartId, Long itemId, Long itemCount) {
+		return new CartItemDto(
 			cartItemId,
 			cartId,
-			itemId
+			itemId,
+			itemCount
 		);
 	}
 
