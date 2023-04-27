bf6e2eaebd642abf7e4bef8b17a2ab57abc26e02
hyejin0662 <hyejin0776@gmail.com>
2023-04-27 14:20:47 +0900

    feat: OrderItem - createOrderItem, getTotalPrice, cancel 메서드 구현

--- service/src/main/java/com/petkpetk/service/domain/shopping/entity/order/OrderItem.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/entity/order/OrderItem.java
@@ -1,14 +1,19 @@
 package com.petkpetk.service.domain.shopping.entity.order;
 
-import java.time.LocalDateTime;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import com.petkpetk.service.common.AuditingFields;
 import com.petkpetk.service.domain.shopping.constant.OrderStatus;
+import com.petkpetk.service.domain.shopping.entity.item.Item;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,21 +25,55 @@ import lombok.ToString;
 @ToString(callSuper = true)
 @NoArgsConstructor
 @Entity
-public class OrderItem {
+public class OrderItem extends AuditingFields {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "order_item_id")
 	private Long id;
-	private Long payId;
-	private Long orderId;
-	private Long userId;
-	private Long itemId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="item_id")
+	@ToString.Exclude
+	private Item item;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "orders_id")
+	@ToString.Exclude
+	private Order order;
+
+	private Long orderPrice; // ** 수정
+
+	private Long orderCount; // ** 수정
+
+	@Enumerated(EnumType.STRING)
 	private OrderStatus orderStatus;
-	private LocalDateTime startTime;
-	private LocalDateTime cancelTime;
+
+	public OrderItem(Item item, Order order, Long orderPrice, Long orderCount, OrderStatus orderStatus) {
+		this.item = item;
+		this.order = order;
+		this.orderPrice = orderPrice;
+		this.orderCount = orderCount;
+		this.orderStatus = orderStatus;
+	}
+
+	public static OrderItem of(Item item, Order order, Long orderPrice, Long orderCount, OrderStatus orderStatus) {
+		return new OrderItem(item, order,orderPrice,orderCount, orderStatus);
+	}
 
 
+	public static OrderItem createOrderItem(Item item, Long stockAmount) {
+		OrderItem orderItem = OrderItem.of(item, null, item.getPrice(),stockAmount,null);
+		item.removeStock(stockAmount);
+		return orderItem;
+	}
 
+	public long getTotalPrice(){
+		return orderPrice * orderCount;
+	}
+
+	public void cancel(){
+		this.getItem().addStock(orderCount);
+	}
 
 }
