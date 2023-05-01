1a984b5bf85a48316419518f466f2c02bfa1806d
KangCuty <goldbaby1@naver.com>
2023-05-01 11:56:39 +0900

    feat: review( 리뷰 ) ReviewRepositoryCustomImpl, ReviewRepositoryCustom 수정

--- service/src/main/java/com/petkpetk/service/domain/shopping/repository/querydsl/reivew/ReviewRepositoryCustom.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/repository/querydsl/reivew/ReviewRepositoryCustom.java
@@ -1,9 +1,5 @@
 package com.petkpetk.service.domain.shopping.repository.querydsl.reivew;
 
-import java.util.List;
-
-import com.petkpetk.service.domain.shopping.dto.review.ManageReviewDto;
-
 public interface ReviewRepositoryCustom {
-	List<ManageReviewDto> getUserReviewList(String email);
+
 }
