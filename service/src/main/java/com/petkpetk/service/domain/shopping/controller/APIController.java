d49aec456d35fe80e59d1fc35943bff7f74bd45c
KangCuty <goldbaby1@naver.com>
2023-05-02 15:33:55 +0900

    feat: main( 메인 ) 상품 데이터들 가져오는 기능 구현

--- service/src/main/java/com/petkpetk/service/domain/shopping/controller/APIController.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/controller/APIController.java
@@ -1,11 +1,25 @@
 package com.petkpetk.service.domain.shopping.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.petkpetk.service.domain.shopping.dto.item.ItemSearchDto;
+import com.petkpetk.service.domain.shopping.dto.item.MainItemDto;
+import com.petkpetk.service.domain.shopping.service.item.ItemService;
 import com.petkpetk.service.domain.shopping.service.review.ReviewService;
 import com.petkpetk.service.domain.shopping.service.review.likes.ReviewLikesService;
+import com.petkpetk.service.domain.user.entity.UserAccount;
+import com.petkpetk.service.domain.user.exception.UserNotFoundException;
+import com.petkpetk.service.domain.user.service.UserAccountService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +30,14 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api")
 public class APIController {
 
-
+	private final UserAccountService userAccountService;
+	private final ItemService itemService;
 	private final ReviewService reviewService;
 	private final ReviewLikesService reviewLikesService;
+	private final PasswordEncoder passwordEncoder;
 
 	@RequestMapping(value = "/review/like", method = RequestMethod.POST)
-	public Boolean like(Long num, Long reviewId, Long likeNum, String userEmail){
+	public Boolean like(Long num, Long reviewId, Long likeNum, String userEmail) {
 
 		if (num == 1) {
 			reviewService.plusLike(reviewId, likeNum);
@@ -37,4 +53,31 @@ public class APIController {
 
 	}
 
+	@RequestMapping("/items")
+	@ResponseBody
+	public Map<String, Object> getItems(ItemSearchDto itemSearchDto, Optional<Integer> page) {
+		Map<String, Object> result = new HashMap<>();
+
+		PageRequest pageRequest = PageRequest.of(page.orElse(0), 12);
+		Page<MainItemDto> items = itemService.getMainItemPage(itemSearchDto, pageRequest);
+
+		Long itemCount = itemService.getItemCount();
+
+		result.put("result", items.getContent());
+		result.put("totalItemCount", itemCount);
+
+		return result;
+	}
+
+	@RequestMapping("checkPass")
+	@ResponseBody
+	public boolean checkPass(String password, String email) {
+		System.out.println("password = " + password);
+		System.out.println("email = " + email);
+		UserAccount userAccount = userAccountService.searchUser(email).orElseThrow(
+				UserNotFoundException::new);
+
+		return userAccount.checkPassword(password, passwordEncoder);
+
+	}
 }
