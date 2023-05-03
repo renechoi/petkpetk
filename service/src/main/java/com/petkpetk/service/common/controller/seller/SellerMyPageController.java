706c884504fc9e6da1f26c4269ac44c0828625fe
KangCuty <goldbaby1@naver.com>
2023-05-03 11:19:42 +0900

    feat: direction( 경로 ) 마이페이지 Controller 공용으로 경로 수정 및 HTML 이동 경로 수정

--- service/src/main/java/com/petkpetk/service/common/controller/seller/SellerMyPageController.java
+++ service/src/main/java/com/petkpetk/service/common/controller/seller/SellerMyPageController.java
@@ -1,4 +1,4 @@
-package com.petkpetk.service.domain.shopping.controller;
+package com.petkpetk.service.common.controller.seller;
 
 import java.util.Optional;
 
@@ -23,13 +23,14 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/seller")
-public class SellerItemController {
+@RequestMapping("/seller/my-page/")
+public class SellerMyPageController {
 	private final UserAccountService userAccountService;
 	private final ItemService itemService;
 
 	@GetMapping("/information")
-	public String informationView(Model model, @AuthenticationPrincipal UserAccountPrincipal userAccountPrincipal) {
+	public String informationView(
+		Model model, @AuthenticationPrincipal UserAccountPrincipal userAccountPrincipal) {
 		model.addAttribute("userAccount", userAccountService.getUserUpdateRequestView(userAccountPrincipal));
 		ProfileImage profileImage = userAccountService.getUserProfile(userAccountPrincipal);
 		model.addAttribute("profileImage", profileImage);
@@ -37,7 +38,8 @@ public class SellerItemController {
 	}
 
 	@GetMapping(value = {"/item-manage", "/item-manage/{page}"})
-	public String itemManage(ItemSearchDto itemSearchDto, @PathVariable("page") Optional<Integer> page, Model model, Authentication authentication){
+	public String itemManage(
+		ItemSearchDto itemSearchDto, @PathVariable("page") Optional<Integer> page, Model model, Authentication authentication){
 		String email = authentication.getName();
 
 		PageRequest pageRequest = PageRequest.of(page.orElse(0), 5);
