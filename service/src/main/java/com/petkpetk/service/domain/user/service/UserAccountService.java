08c80265fdde2ba2f7973e508fbfac8ca39c73ed
renechoi <115696395+renechoi@users.noreply.github.com>
2023-05-03 14:01:00 +0900

    refactor: 업데이트 뷰 리턴 메서드 문법 수정

--- service/src/main/java/com/petkpetk/service/domain/user/service/UserAccountService.java
+++ service/src/main/java/com/petkpetk/service/domain/user/service/UserAccountService.java
@@ -61,14 +61,10 @@ public class UserAccountService {
 	public UserUpdateRequest getUserUpdateRequestView(UserAccountPrincipal userAccountPrincipal) {
 		UserAccount userAccount = findByEmail(userAccountPrincipal.getEmail()).orElseThrow(UserNotFoundException::new);
 
-		if (userAccountPrincipal.getProfileImage() != null) {
-			MultipartFile profileRawImage =
-				userAccountPrincipal instanceof OAuth2UserAccountPrincipal ? null :
-					imageLocalRepository.findByPetkpetkImage(userAccount.getProfileImage());
-			return UserUpdateRequest.from(userAccount, profileRawImage);
-		} else {
-			return UserUpdateRequest.from(userAccount);
-		}
+		return userAccountPrincipal.getProfileImage() != null ?
+			UserUpdateRequest.from(userAccount, userAccountPrincipal instanceof OAuth2UserAccountPrincipal ? null :
+				imageLocalRepository.findByPetkpetkImage(userAccount.getProfileImage())) :
+			UserUpdateRequest.from(userAccount);
 
 	}
 
@@ -115,7 +111,8 @@ public class UserAccountService {
 	}
 
 	public ProfileImage getUserProfile(UserAccountPrincipal userAccountPrincipal) {
-		return profileImageRepository.findByUserAccountId(userAccountPrincipal.getId()).orElseThrow(UserNotFoundException::new);
+		return profileImageRepository.findByUserAccountId(userAccountPrincipal.getId())
+			.orElseThrow(UserNotFoundException::new);
 	}
 
 	public boolean isPasswordSame(String password, String email) {
