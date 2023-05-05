fcf89852aca630d642147dbc6fa8255524491941
hyejin0662 <hyejin0776@gmail.com>
2023-05-05 14:23:53 +0900

    feat: order PostMapping 구현

--- service/src/main/java/com/petkpetk/service/domain/shopping/controller/OrderController.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/controller/OrderController.java
@@ -1,7 +1,6 @@
 package com.petkpetk.service.domain.shopping.controller;
 
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -13,12 +12,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.petkpetk.service.domain.shopping.dto.order.OrderHistoryDto;
@@ -34,42 +30,22 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/user/order") // 수정 예정
+@RequestMapping("/order")
 public class OrderController {
 
 	private final OrderService orderService;
 
-	@GetMapping("")
-	public String order(Model model){
-		model.addAttribute("orderRequest", new OrderRequest());
-		return "order-test";
-	}
-
-
 	@PostMapping("")
-	public ResponseEntity<Object> createOrder(@RequestBody @Valid  OrderRequest orderRequest,
-								BindingResult bindingResult,
-								@AuthenticationPrincipal UserAccountPrincipal userAccountPrincipal){
-		// todo :
-		//  principal -> userAccountDto 만들어서
-		// item에 담아주고
-
-		if(bindingResult.hasErrors()) {
-			String errorMsg = bindingResult.getAllErrors().stream()
-				.map(ObjectError::getDefaultMessage)
-				.collect(Collectors.joining(", "));
-			return ResponseEntity.badRequest().body(errorMsg);
-
-		}
-		try {
-		Long orderId = orderService.createOrder(orderRequest, userAccountPrincipal.toDto().getEmail());
-			return ResponseEntity.ok(orderId);
-		} catch (Exception e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
+	public String order(@Valid OrderRequest orderRequest, Model model,
+		@AuthenticationPrincipal UserAccountPrincipal userAccountPrincipal){
 
 
 
+		//todo : 결제 기능 구현
+
+		Long orderId = orderService.createOrder(orderRequest, userAccountPrincipal.getEmail());
+
+		return "payment/complete";
 	}
 
 
