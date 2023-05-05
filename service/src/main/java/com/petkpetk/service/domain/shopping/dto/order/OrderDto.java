04d21d12312141675a76ee6c08a7c1d574b72af2
hyejin0662 <hyejin0776@gmail.com>
2023-05-05 14:28:52 +0900

    rename: OrderDto 필드명 변경

--- service/src/main/java/com/petkpetk/service/domain/shopping/dto/order/OrderDto.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/dto/order/OrderDto.java
@@ -1,11 +1,7 @@
 package com.petkpetk.service.domain.shopping.dto.order;
 
-import java.time.LocalDateTime;
-
 import com.petkpetk.service.config.converter.EntityAndDtoConverter;
-import com.petkpetk.service.domain.shopping.entity.item.Item;
 import com.petkpetk.service.domain.shopping.entity.order.Order;
-import com.petkpetk.service.domain.shopping.entity.order.OrderItem;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,8 +16,8 @@ public class OrderDto {
 
 	private Long userId;
 
-	private Long productId;
-	private Long amount;
+	private Long ItemId;
+	private Long orderCount;
 	private Long orderPrice;
 	private double salePercent;
 	private Long salePrice;
@@ -33,19 +29,19 @@ public class OrderDto {
 		return EntityAndDtoConverter.convertToDto(order, OrderDto.class);
 	}
 
-	public OrderDto(Long id, Long userId, Long productId, Long amount, double salePercent) {
+	public OrderDto(Long id, Long userId, Long ItemId, Long orderCount, double salePercent) {
 		this.id = id;
 		this.userId = userId;
-		this.productId = productId;
-		this.amount = amount;
+		this.ItemId = ItemId;
+		this.orderCount = orderCount;
 		this.salePercent = salePercent;
 	}
 
-	public OrderDto(Long id, Long userId, Long productId, Long amount, Long salePrice) {
+	public OrderDto(Long id, Long userId, Long ItemId, Long orderCount, Long salePrice) {
 		this.id = id;
 		this.userId = userId;
-		this.productId = productId;
-		this.amount = amount;
+		this.ItemId = ItemId;
+		this.orderCount = orderCount;
 		this.salePrice = salePrice;
 	}
 
