754d382671055c442d8c5d1a748c8d59cdae634c
hyejin0662 <hyejin0776@gmail.com>
2023-05-05 14:29:39 +0900

    rename: CheckoutRequest 필드명 변경

--- service/src/main/java/com/petkpetk/service/domain/shopping/controller/CheckoutController.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/controller/CheckoutController.java
@@ -1,29 +1,14 @@
 package com.petkpetk.service.domain.shopping.controller;
 
-import java.util.Optional;
-
 import javax.validation.Valid;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.petkpetk.service.domain.shopping.dto.order.OrderHistoryDto;
-import com.petkpetk.service.domain.shopping.dto.order.request.CheckoutReqeust;
-import com.petkpetk.service.domain.shopping.entity.delivery.Delivery;
-import com.petkpetk.service.domain.shopping.entity.order.Order;
-import com.petkpetk.service.domain.shopping.service.item.ItemService;
+import com.petkpetk.service.domain.shopping.dto.order.request.CheckoutRequest;
 import com.petkpetk.service.domain.shopping.service.order.OrderService;
-import com.petkpetk.service.domain.user.dto.security.UserAccountPrincipal;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -37,9 +22,9 @@ public class CheckoutController {
 	private final OrderService orderService;
 
 	@PostMapping("")
-	public String checkout(@Valid CheckoutReqeust checkoutReqeust, Model model){
+	public String checkout(@Valid CheckoutRequest checkoutRequest, Model model){
 		//todo : item 찾아오기
-		model.addAttribute("item", orderService.createCheckOut(checkoutReqeust));
+		model.addAttribute("item", orderService.createCheckOut(checkoutRequest));
 		return "order/checkout";
 	}
 
