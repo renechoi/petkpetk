53053d8c3a23d055360d6a1256a03f881489262d
hyejin0662 <hyejin0776@gmail.com>
2023-05-06 13:39:05 +0900

    refactor: cartItemCount 로직 추가를 위해 조인 테이블 명시적 방법으로 매핑하도록 구현 수정

--- service/src/main/java/com/petkpetk/service/domain/shopping/entity/cart/CartItem.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/entity/cart/CartItem.java
@@ -1,75 +1,54 @@
-// package com.petkpetk.service.domain.shopping.entity.cart;
-//
-// import javax.persistence.Entity;
-// import javax.persistence.GeneratedValue;
-// import javax.persistence.GenerationType;
-// import javax.persistence.Id;
-// import javax.persistence.JoinColumn;
-// import javax.persistence.ManyToOne;
-// import javax.persistence.Table;
-//
-// import com.petkpetk.service.common.AuditingFields;
-// import com.petkpetk.service.domain.shopping.entity.item.Item;
-//
-// import lombok.AllArgsConstructor;
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.Setter;
-// import lombok.ToString;
-//
-// @Getter
-// @Setter
-// @ToString(callSuper = true)
-// @NoArgsConstructor
-// @AllArgsConstructor
-// @Entity
-// @Table(name = "cart_item")
-// public class CartItem extends AuditingFields {
-//
-// 	@Id
-// 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-// 	private Long id;
-//
-// 	@ManyToOne
-// 	@JoinColumn(name = "cart_id")
-// 	private Cart cart;
-//
-// 	@ManyToOne
-// 	@JoinColumn(name = "item_id")
-// 	private Item item;
-//
-// 	private Long itemCount;
-//
-// 	// private List<OrderItemDto> orderItemList = new ArrayList<>();
-// 	//
-//
-// 	private Long totalPrice;
-//
-// 	public CartItem(Cart cart, Item item, Long itemCount, Long totalPrice) {
-// 		this.cart = cart;
-// 		this.item = item;
-// 		this.itemCount = itemCount;
-// 		this.totalPrice = totalPrice;
-// 	}
-//
-//
-// 	public static CartItem of(Cart cart, Item item, Long itemCount, Long totalPrice) {
-// 		return new CartItem(cart, item, itemCount,totalPrice);
-// 	}
-//
-// 	public static CartItem createCartItem(Cart cart, Item item, Long itemCount, Long totalPrice) {
-// 		return CartItem.of(cart,item,itemCount, totalPrice);
-// 	}
-//
-// 	public void addItemCount(Long itemCount){
-// 		this.itemCount += itemCount;
-// 	}
-//
-// 	public void updateCount(Long itemCount){
-// 		this.itemCount = itemCount;
-// 	}
-//
-// }
-//
-//
-//
+package com.petkpetk.service.domain.shopping.entity.cart;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.petkpetk.service.domain.shopping.entity.item.Item;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cart_item")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @Column(name = "cart_item_count")
+    private Long cartItemCount;
+
+    public CartItem(Cart cart, Item item, Long cartItemCount) {
+        this.cart = cart;
+        this.item = item;
+        this.cartItemCount = cartItemCount;
+    }
+
+    public static CartItem of(Cart cart, Item item, Long cartItemCount) {
+        return new CartItem(cart, item, cartItemCount);
+    }
+}
--- service/src/main/java/com/petkpetk/service/domain/shopping/repository/cart/CartItemRepository.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/repository/cart/CartItemRepository.java
@@ -1,28 +1,15 @@
-// package com.petkpetk.service.domain.shopping.repository.cart;
-//
-// import java.util.List;
-// import java.util.Optional;
-//
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Query;
-// import org.springframework.stereotype.Repository;
-//
-// import com.petkpetk.service.domain.shopping.dto.cart.CartDetailDto;
-// import com.petkpetk.service.domain.shopping.dto.cart.CartItemDto;
-// import com.petkpetk.service.domain.shopping.entity.cart.Cart;
-// import com.petkpetk.service.domain.shopping.entity.cart.CartItem;
-//
-//
-// @Repository
-// public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-//
-// 	Optional<CartItem> findByCartIdAndItemId(Long cartId, Long itemId);
-//
-// 	@Query("select new com.petkpetk.service.domain.shopping.dto.cart.CartDetailDto(cartItem.id, item.itemName, item.price, cartItem.itemCount, itemImage.imageUrl) from CartItem cartItem, ItemImage itemImage join cartItem.item item where cartItem.cart.id =:cartId and itemImage.item.id = cartItem.item.id and itemImage.representativeImageYn = 'Y' order by cartItem.createdAt desc")
-// 	List<CartDetailDto> findCartDetailDtos(Long cartId);
-//
-//
-// 	List<CartItem> deleteAllByIdIn(List<Long> cartIds);
-//
-// 	Cart findByUserAccountId(Long id);
-// }
+package com.petkpetk.service.domain.shopping.repository.cart;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.petkpetk.service.domain.shopping.entity.cart.CartItem;
+
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+	Optional<CartItem> findByCartIdAndItemId(Long cartId, Long itemId);
+
+}
