8657138a75cc8e7d240c412d3f0e4f9b8ce7f2e5
hyejin0662 <hyejin0776@gmail.com>
2023-05-06 11:39:04 +0900

    feat: 장바구니 cartItem 테이블 삭제 및 장바구니 기능 구현

--- service/src/main/java/com/petkpetk/service/domain/shopping/entity/cart/Cart.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/entity/cart/Cart.java
@@ -1,5 +1,11 @@
 package com.petkpetk.service.domain.shopping.entity.cart;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -7,9 +13,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.petkpetk.service.common.AuditingFields;
+import com.petkpetk.service.domain.community.entity.Hashtag;
+import com.petkpetk.service.domain.shopping.dto.item.ItemDto;
+import com.petkpetk.service.domain.shopping.entity.item.Item;
+import com.petkpetk.service.domain.user.dto.UserAccountDto;
 import com.petkpetk.service.domain.user.entity.UserAccount;
 
 import lombok.AllArgsConstructor;
@@ -37,17 +50,22 @@ public class Cart  extends AuditingFields {
 
 	private Long totalPrice;
 
+	@ToString.Exclude
+	@JoinTable(name = "cart_item", joinColumns = @JoinColumn(name = "cart_id"), inverseJoinColumns = @JoinColumn(name = "item_id"))
+	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	private Set<Item> items = new LinkedHashSet<>();
+
+
 	private Cart(UserAccount userAccount) {
 		this.userAccount = userAccount;
 	}
 
-	public static Cart of(UserAccount userAccount) {
-		return new Cart(userAccount);
+	public static Cart of(UserAccountDto userAccountDto) {
+		return new Cart(userAccountDto.toEntity());
 	}
 
-
-	public static Cart createCart(UserAccount userAccount){
-			return Cart.of(userAccount);
+	public void addItem(Item item) {
+		this.items.add(item);
+		item.getCarts().add(this);
 	}
-
 }
