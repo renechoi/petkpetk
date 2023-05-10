c127435728d32e6270a96845f48200d7330eaeee
hyejin0662 <hyejin0776@gmail.com>
2023-05-10 19:58:39 +0900

    feat: ItemRepository - findItemWithRepresentativeImageById 구현

--- service/src/main/java/com/petkpetk/service/domain/shopping/repository/item/ItemRepository.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/repository/item/ItemRepository.java
@@ -1,10 +1,12 @@
 package com.petkpetk.service.domain.shopping.repository.item;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 import com.petkpetk.service.domain.shopping.entity.item.Item;
 import com.petkpetk.service.domain.shopping.repository.querydsl.item.ItemRepositoryCustom;
@@ -20,4 +22,12 @@ public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredi
 
 	@Query("select count(t) from Item t where t.deletedYn='N'")
 	Long getItemCount();
+
+	@Query("SELECT i FROM Item i JOIN i.images img WHERE img.representativeImageYn = 'Y' AND i.id = :itemId")
+	Optional<Item> findItemWithRepresentativeImage(@Param("itemId") Long itemId);
+
+	Optional<Item> findByIdAndImagesRepresentativeImageYn(Long itemId, String representativeImageYn);
+
+	@Query("SELECT i FROM Item i JOIN FETCH i.images img WHERE img.representativeImageYn = 'Y' AND i.id = :itemId")
+	Optional<Item> findItemWithRepresentativeImageById(@Param("itemId") Long itemId);
 }
