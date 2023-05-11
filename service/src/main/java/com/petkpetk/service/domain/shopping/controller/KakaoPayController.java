40d0d78355d00c41d321c20380207b277a247f03
hyejin0662 <hyejin0776@gmail.com>
2023-05-11 11:16:07 +0900

    feat: KakaoPayController - 결제진행중 취소, 결제실패, 환불 로직구현

--- service/src/main/java/com/petkpetk/service/domain/shopping/controller/KakaoPayController.java
+++ service/src/main/java/com/petkpetk/service/domain/shopping/controller/KakaoPayController.java
@@ -1,10 +1,17 @@
 package com.petkpetk.service.domain.shopping.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.petkpetk.service.common.StatusCode;
+import com.petkpetk.service.domain.shopping.dto.payment.KakaoCancelResponse;
 import com.petkpetk.service.domain.shopping.dto.payment.PaymentRequest;
+import com.petkpetk.service.domain.shopping.exception.CancellationInProgressException;
+import com.petkpetk.service.domain.shopping.exception.PaymentFailException;
 import com.petkpetk.service.domain.shopping.service.payment.KakaoPayService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,4 +28,33 @@ public class KakaoPayController {
 		 return String.format("redirect:%s", kakaoPayService.kakaoPayReady(paymentRequest).getNext_redirect_pc_url());
 	}
 
+	/**
+	 * 결제 진행 중 취소
+	 */
+	@GetMapping("/cancel")
+	public void cancel() {
+
+		throw new CancellationInProgressException(StatusCode.PAY_CANCEL);
+	}
+
+	/**
+	 * 결제 실패
+	 */
+	@GetMapping("/fail")
+	public void fail() {
+
+		throw new PaymentFailException(StatusCode.PAY_FAILED);
+	}
+	/**
+	 * 환불
+	 */
+	@PostMapping("/refund")
+	public ResponseEntity refund() {
+
+		KakaoCancelResponse kakaoCancelResponse = kakaoPayService.kakaoCancel();
+
+		return new ResponseEntity<>(kakaoCancelResponse, HttpStatus.OK);
+	}
+
 }
+
