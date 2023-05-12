83c3b45cf71cbbc8b3b713a9afda60bcc1c932a2
renechoi <115696395+renechoi@users.noreply.github.com>
2023-05-12 19:55:13 +0900

    fix: deploy bashrc로 의존성 주입 받기

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,11 +21,16 @@ fi
 
 
 
-
+source
+ ~/.bashrc
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/admin/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/admin/deploy_err.log &
+#nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/admin/deploy_err.log &
+
+nohup java -jar $DEPLOY_JAR -Dspring.profiles.active=prod --spring.config.location=/home/ec2-user/action/admin/src/main/resources/application-prod.yml >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/admin/deploy_err.log &
+
+
 
 #export
 # SPRING_PROFILES_ACTIVE=prod
